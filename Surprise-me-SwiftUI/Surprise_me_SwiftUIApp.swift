2c71ebb5f3c32b73eec5e6187a9ad30559a34c0a
Bruno Madeira <madeirabruno624@gmail.com>
2024-06-11 17:28:42 +0100

    Initial Commit

---
+++ Surprise-me-SwiftUI/Surprise_me_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Surprise_me_SwiftUIApp.swift
+//  Surprise-me-SwiftUI
+//
+//  Created by Bruno Madeira on 11/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Surprise_me_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
