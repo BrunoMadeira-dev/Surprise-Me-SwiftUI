faa57633a20cae34c5a3aeff0b0596ac98bf5b36
Bruno Madeira <madeirabruno624@gmail.com>
2024-06-11 18:46:16 +0100

    Created new App on SwiftUI. Added splash screen with new animation

---
+++ Surprise-me-SwiftUI/SplashScreenView.swift
@@ -0,0 +1,47 @@
+//
+//  SplashScreenView.swift
+//  Surprise-me-SwiftUI
+//
+//  Created by Bruno Madeira on 11/06/2024.
+//
+
+import SwiftUI
+
+struct SplashScreenView: View {
+    
+    @State private var isActive = false
+    @State private var size = 0.8
+    @State private var opacity = 0.5
+    var body: some View {
+        
+        if isActive {
+            SurpriseMe()
+        } else {
+            VStack {
+                VStack {
+                    Image("surprise-me-logo" )
+                        .resizable()
+                        .scaledToFit()
+                        .foregroundStyle(Color("lightBlueColor"))
+                }
+                .scaleEffect(size)
+                .opacity(opacity)
+                .onAppear {
+                    withAnimation(.easeIn(duration: 1.2)) {
+                        self.size = 0.9
+                        self.opacity = 1.0
+                    }
+                }
+            }
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+                    self.isActive = true
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    SplashScreenView()
+}
--- Surprise-me-SwiftUI/Surprise_me_SwiftUIApp.swift
+++ Surprise-me-SwiftUI/Surprise_me_SwiftUIApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Surprise_me_SwiftUIApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            SplashScreenView()
         }
     }
 }
